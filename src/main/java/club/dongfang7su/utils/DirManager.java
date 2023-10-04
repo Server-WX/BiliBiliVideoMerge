b026cef4a54ecbd9e778cfcbd3c1fa80c08a383d
Server-WX <lfl3518963655@163.com>
2023-10-04 14:01:23 +0800

    文件夹处理模块

---
+++ src/main/java/club/dongfang7su/utils/DirManager.java
@@ -0,0 +1,27 @@
+package club.dongfang7su.utils;
+
+import java.io.File;
+
+public class DirManager {
+
+    private String dirPath;
+
+    public DirManager() {
+    }
+
+    public File[] getDirPathList() {
+
+        File path = new File(this.dirPath);
+        if (!path.exists()) {
+            System.out.println("非法路径！");
+            return null;
+        }
+
+        return path.listFiles();
+
+    }
+
+    public void setDirPath(String dirPath) {
+        this.dirPath = dirPath;
+    }
+}
