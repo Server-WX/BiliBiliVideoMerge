85ed897c10e5843886a6da7f300d904819eb28c0
Server-WX <lfl3518963655@163.com>
2023-10-05 15:46:32 +0800

    优化交互逻辑

--- src/main/java/club/dongfang7su/Main.java
+++ src/main/java/club/dongfang7su/Main.java
@@ -6,6 +6,7 @@ import club.dongfang7su.utils.SaveFile;
 import club.dongfang7su.utils.VideoMerge;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.Scanner;
 
 /*
@@ -18,15 +19,26 @@ public class Main {
 
     public static void main(String[] args) {
         String dirPath = null;
+        Scanner sc = new Scanner(System.in);
+        final String NVIDIA_GPU = "NVIDIA_GPU";
+        final String INTEL_GPU = "INTEL_GPU";
+        final String SOFTWARE_CPU = "SOFTWARE_CPU";
 
         for (String arg : args) {
             if (new File(arg).exists()) {
                 dirPath = arg.replace("\\", "\\\\");     //  读取拖拽文件路径
             }
         }
-//        dirPath = "D:\\桌面\\我的三体-章北海传";
+//        dirPath = "D:\\桌面\\s_45570";
+
         DirManager dirManager = new DirManager();
-        dirManager.setDirPath(dirPath);     //  初始化路径管理器，进入文件夹
+        if (dirPath != null) {
+            System.out.println("加载文件中");
+            System.out.println();
+            dirManager.setDirPath(dirPath);     //  初始化路径管理器，进入文件夹
+            System.out.println("视频总数：" + Objects.requireNonNull(new File(dirPath).listFiles()).length);
+            System.out.println();
+        }
 
         GetJsonData jsonData = new GetJsonData(dirManager.getDirPathList());    //  读取entry.json中的数据
         //  获取番剧名称、集数，标题
@@ -36,15 +48,52 @@ public class Main {
                 jsonData.getIndexTitleList()
         );
 
+        System.out.println("加载完毕，请选择编码器：");
+        System.out.println();
+        System.out.println("输入对应数字回车即可：");
+        System.out.println();
+        System.out.println("1.  英伟达独显加速");
+        System.out.println("2.  英特尔核显加速");
+        System.out.println("3.  CPU软件编码");
+        System.out.println();
         //  合并视频文件，输出同级目录下
-        new SaveFile(
-                dirPath,
-                dirManager.getDirPathList(),
-                jsonData.getVideoName(),
-                videoMerge.getFileNameList()
-        );
+        int ifNumber = sc.nextInt();
+        long startTime = System.currentTimeMillis();
+        switch (ifNumber) {
+            case 1:
+                new SaveFile(
+                        dirPath,
+                        dirManager.getDirPathList(),
+                        jsonData.getVideoName(),
+                        videoMerge.getFileNameList(),
+                        NVIDIA_GPU
+                );
+                break;
+            case 2:
+                new SaveFile(
+                        dirPath,
+                        dirManager.getDirPathList(),
+                        jsonData.getVideoName(),
+                        videoMerge.getFileNameList(),
+                        INTEL_GPU
+                );
+                break;
+            case 3:
+                new SaveFile(
+                        dirPath,
+                        dirManager.getDirPathList(),
+                        jsonData.getVideoName(),
+                        videoMerge.getFileNameList(),
+                        SOFTWARE_CPU
+                );
+                break;
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println();
+        System.out.println("处理完毕，耗时：" + (endTime - startTime) / 1000.0 + "秒");
+        System.out.println();
 
-        Scanner sc = new Scanner(System.in);    //  回车退出程序
-        sc.nextLine();
+        System.out.println("按下回车键退出程序");
+        sc.next();    //  回车退出程序
     }
 }
