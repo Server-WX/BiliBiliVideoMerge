e2587281f013050cd7ed304c11d47c5c8bd42285
Server-WX <lfl3518963655@163.com>
2023-10-05 15:47:06 +0800

    新增转码H.264，提供三种编码器

--- src/main/java/club/dongfang7su/utils/SaveFile.java
+++ src/main/java/club/dongfang7su/utils/SaveFile.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SaveFile {
-    public SaveFile(String dirPath, File[] filesPath, String dirName, ArrayList<String> fileNameList) {
+    public SaveFile(String dirPath, File[] filesPath, String dirName, ArrayList<String> fileNameList, String encoder) {
         File path = new File(dirPath);
         System.out.println(path.getPath() + " " + path.exists());
         File savePath = new File(path.getParentFile() + "\\" + dirName);
@@ -25,28 +25,48 @@ public class SaveFile {
             String videoFile = mediaFilePath + "video.m4s";
             String audioFile = mediaFilePath + "audio.m4s";
 
-            String[] command = {"\"" + ffmpegPath + "\"" + " -i " + "\"" + videoFile + "\"" + " -i " + "\"" + audioFile + "\"" + " -codec copy " + "\"" + output + "\""};
-            System.out.println(Arrays.toString(command));
-            try {
-                Process videoProcess = new ProcessBuilder(command).redirectErrorStream(true).start();
-                // 获取输出流
-                InputStream inputStream = videoProcess.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String[] NVIDIA_GPU = {"\"" + ffmpegPath + "\"" + " -i " + "\"" + videoFile + "\"" + " -i " + "\"" + audioFile + "\"" + " -codec copy -c:v h264_nvenc " + "\"" + output + "\""};
+            String[] INTEL_GPU = {"\"" + ffmpegPath + "\"" + " -i " + "\"" + videoFile + "\"" + " -i " + "\"" + audioFile + "\"" + " -codec copy -c:v h264_qsv " + "\"" + output + "\""};
+            String[] SOFTWARE_CPU = {"\"" + ffmpegPath + "\"" + " -i " + "\"" + videoFile + "\"" + " -i " + "\"" + audioFile + "\"" + " -codec copy -c:v libx264 " + "\"" + output + "\""};
+//            String[] command = {"\"" + ffmpegPath + "\"" + " -i " + "\"" + videoFile + "\"" + " -i " + "\"" + audioFile + "\"" + " -codec copy -c:v h264_nvenc " + "\"" + output + "\""};
 
-                // 读取输出内容并打印
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
-                }
-                int exitCode = videoProcess.waitFor();
-                if (exitCode == 0) {
-                    System.out.println("合并成功，文件：" + output);
-                } else {
-                    System.out.println("合并失败，错误代码：" + exitCode);
-                }
-            } catch (IOException | InterruptedException e) {
-                throw new RuntimeException(e);
+            switch (encoder) {
+                case "NVIDIA_GPU":
+                    outputFile(NVIDIA_GPU, output);
+                    break;
+                case "INTEL_GPU":
+                    outputFile(INTEL_GPU, output);
+                    break;
+                case "SOFTWARE_CPU":
+                    outputFile(SOFTWARE_CPU, output);
+                    break;
             }
+
         }
     }
+
+    private static void outputFile(String[] command, String output) {
+        try {
+            System.out.println(Arrays.toString(command));
+            Process videoProcess = new ProcessBuilder(command).redirectErrorStream(true).start();
+            // 获取输出流
+            InputStream inputStream = videoProcess.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // 读取输出内容并打印
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            int exitCode = videoProcess.waitFor();
+            if (exitCode == 0) {
+                System.out.println("合并成功，文件：" + output);
+            } else {
+                System.out.println("合并失败，错误代码：" + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
