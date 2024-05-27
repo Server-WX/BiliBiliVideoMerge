82e0cb75eb3e0f26cb003e689c8f1efe5b1827e6
Server-WX <lfl3518963655@163.com>
2024-05-28 03:36:05 +0800

    适配对非正常的entry.json检测以解决程序崩溃

--- src/main/java/club/dongfang7su/utils/VideoMerge.java
+++ src/main/java/club/dongfang7su/utils/VideoMerge.java
@@ -7,12 +7,12 @@ public class VideoMerge {
 
     private final ArrayList<String> fileNameList = new ArrayList<>();
 
-    public VideoMerge(File[] dirNumber, ArrayList<String> indexList, ArrayList<String> indexTitleList) {
+    public VideoMerge(ArrayList<String> indexList, ArrayList<String> indexTitleList) {
 
-        for (int i = 0; i < dirNumber.length; i++) {
-            String indexVideo = "第" + indexList.get(i) + "集";
-            String videoTitle = indexTitleList.get(i).replace(" ", "_");
-            if (indexTitleList.get(i).equals("")) {
+        for (String index : indexList) {
+            String indexVideo = "第" + index + "集";
+            String videoTitle = indexTitleList.get(indexList.indexOf(index)).replace(" ", "_");
+            if (indexTitleList.get(indexList.indexOf(index)).isEmpty()) {
                 this.fileNameList.add(indexVideo);
             } else this.fileNameList.add(indexVideo + "-" + videoTitle);
         }
