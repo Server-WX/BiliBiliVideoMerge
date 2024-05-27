7350929e89c5ef7c3cc705b993034dd6aa0c2f3a
Server-WX <lfl3518963655@163.com>
2024-05-28 03:35:29 +0800

    新增用户视频缓存合并 新增非正常的entry.json检测 优化报错信息，解决程序崩溃

--- src/main/java/club/dongfang7su/utils/GetJsonData.java
+++ src/main/java/club/dongfang7su/utils/GetJsonData.java
@@ -7,30 +7,60 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GetJsonData {
 
     private final ArrayList<String> indexList = new ArrayList<>();
     private final ArrayList<String> indexTitleList = new ArrayList<>();
+    private final ArrayList<File> availableFileList = new ArrayList<>();
 
     private String videoName = null;
 
+
+    private static JsonObject getReader(File file) throws FileNotFoundException {
+        JsonObject jsonObject;
+        JsonReader reader = Json.createReader(new FileReader(file.getPath() + "\\entry.json"));
+        jsonObject = reader.readObject();
+        return jsonObject;
+    }
+
     public GetJsonData(File[] dirPathList) {
-        try {
-            JsonObject jsonObject = null;
-            for (File file : dirPathList) {
-                JsonReader reader = Json.createReader(new FileReader(file.getPath() + "\\entry.json"));
-                jsonObject = reader.readObject();
-                this.indexList.add(jsonObject.getJsonObject("ep").getString("index"));
-                this.indexTitleList.add(jsonObject.getJsonObject("ep").getString("index_title"));
-            }
-            if (jsonObject != null) {
-                this.videoName = jsonObject.getString("title").replace(" ", "_");
+
+        Scanner scanner = new Scanner(System.in);
+        String cartoon = "ep";
+        String userVideo = "page_data";
+
+        for (File file : dirPathList) {
+            try {
+                if (!new File(file.getPath() + "\\entry.json").exists())
+                    System.out.print(file.getPath());
+
+                JsonObject jsonObject = getReader(file);
+
+                this.videoName = getReader(file).getString("title").replace(" ", "_");
+                if (jsonObject.getJsonObject(cartoon) != null) {
+                    this.indexList.add(jsonObject.getJsonObject(cartoon).getString("index"));
+                    this.indexTitleList.add(jsonObject.getJsonObject(cartoon).getString("index_title"));
+                } else if (jsonObject.getJsonObject(userVideo) != null) {
+                    this.indexList.add(jsonObject.getJsonObject(userVideo).getString("download_subtitle"));
+                    this.indexTitleList.add(jsonObject.getJsonObject(userVideo).getString("part"));
+                } else
+                    throw new NullPointerException();
+                this.availableFileList.add(file);
+
+            } catch (NullPointerException | FileNotFoundException e) {
+                System.out.println(" 视频信息读取失败,原因可能如下："
+                        + "\n" + "entry.json文件不存在"
+                        + "\n" + "未知的entry.json文件，仅支持B站移动端缓存方式");
+                System.out.println("\n是否跳过此视频？如果否则退出程序（请输入y/n）");
+                String ch = scanner.nextLine();
+                if (!ch.equalsIgnoreCase("y"))
+                    ExitProgram.exit();
             }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         }
+
     }
 
     public ArrayList<String> getIndexList() {
@@ -43,5 +73,9 @@ public class GetJsonData {
 
     public String getVideoName() {
         return this.videoName;
+    }
+
+    public ArrayList<File> getAvailableFileList() {
+        return this.availableFileList;
     }
 }
