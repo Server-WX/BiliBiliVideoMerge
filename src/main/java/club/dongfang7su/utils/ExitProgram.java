049347fe1237fb9249b15b3ea6e05ce82288dd15
Server-WX <dongfang_qisu@163.com>
2025-05-11 03:49:32 +0800

    补充注释

--- src/main/java/club/dongfang7su/utils/ExitProgram.java
+++ src/main/java/club/dongfang7su/utils/ExitProgram.java
@@ -2,8 +2,13 @@ package club.dongfang7su.utils;
 
 import java.util.Scanner;
 
+/*
+*
+* 自定义回车退出
+*
+* */
+
 public class ExitProgram {
-    //  回车退出程序
     public static void exit() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("回车退出程序\n");
